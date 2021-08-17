d0ca8f9c8155df0803af9b07daf8a0f847bf954d
Dima Panov <fluffy@FreeBSD.org>
2021-08-17 19:46:19 +1000

    Welcome Boost 1.77.0 release!

--- devel/boost-all/common.mk
+++ devel/boost-all/common.mk
@@ -1,5 +1,5 @@
 
-PORTVERSION=	1.76.0
+PORTVERSION=	1.77.0
 PORTVERSUFFIX=	${PORTVERSION:C/\.[0-9]+$//}
 DISTNAME=	boost_${PORTVERSION:S/./_/g}
 
@@ -8,5 +8,8 @@ MAINTAINER=	office@FreeBSD.org
 
 MASTER_SITES=	https://dl.bintray.com/boostorg/release/${PORTVERSION}/source/ \
 		SF/boost/boost/${PORTVERSION}
+
+LICENSE=	BSL
+LICENSE_FILE=	${WRKSRC}/LICENSE_1_0.txt
 
 USES+=		tar:bzip2
