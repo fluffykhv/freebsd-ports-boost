18dd73839d9055bd12129eeb6d3b0f692c5c2267
Dima Panov <fluffy@FreeBSD.org>
2022-04-22 12:42:49 +0300

    devel/py-llvmlite: update to 0.38.0

--- devel/py-llvmlite/files/patch-ffi_build.py
+++ devel/py-llvmlite/files/patch-ffi_build.py
@@ -1,11 +1,11 @@
---- ffi/build.py.orig	2021-02-26 12:43:25 UTC
+--- ffi/build.py.orig	2022-01-13 16:47:16 UTC
 +++ ffi/build.py
 @@ -163,7 +163,7 @@ def main_posix(kind, library_ext):
          print(warning + '\n')
      else:
  
--        if not (out.startswith('10.0.') or out.startswith('9.0')):
-+        if not (out.startswith('11.0.') or out.startswith('10.0.') or out.startswith('9.0')):
-             msg = ("Building llvmlite requires LLVM 10.0.x or 9.0.x, got "
+-        if not out.startswith('11'):
++        if not (out.startswith('14') or out.startswith('13') or out.startswith('12') or out.startswith('11')):
+             msg = ("Building llvmlite requires LLVM 11.x.x, got "
                     "{!r}. Be sure to set LLVM_CONFIG to the right executable "
                     "path.\nRead the documentation at "
