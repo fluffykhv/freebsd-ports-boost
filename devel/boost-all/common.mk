b04f5dee2d4a85365f8893f16b9730773df3d4aa
Dima Panov <fluffy@FreeBSD.org>
2022-07-20 12:50:07 +0300

    Boost 1.80.0.b1

--- devel/boost-all/common.mk
+++ devel/boost-all/common.mk
@@ -1,12 +1,15 @@
 
-PORTVERSION=	1.79.0
+PORTVERSION=	1.80.0.b1
 PORTVERSUFFIX=	${PORTVERSION:C/\.[0-9]+$//}
 DISTNAME=	boost_${PORTVERSION:S/./_/g}
+
+DISTINFO_FILE?=	${.CURDIR}/../boost-all/distinfo
 
 CATEGORIES=	devel
 MAINTAINER=	office@FreeBSD.org
 
 MASTER_SITES=	https://boostorg.jfrog.io/artifactory/main/release/${PORTVERSION}/source/ \
+		https://boostorg.jfrog.io/artifactory/main/beta/${PORTVERSION:S/.b/.beta/g}/source/ \
 		SF/boost/boost/${PORTVERSION}
 
 USES+=		tar:bzip2
