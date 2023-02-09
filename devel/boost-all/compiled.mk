ed21fcbb09c95ffdb7b97fe89b3d32e74e98a661
Dima Panov <fluffy@FreeBSD.org>
2023-02-09 10:37:34 +0300

    FreeBSD main have imported llvm-15 at 1400079

--- devel/boost-all/compiled.mk
+++ devel/boost-all/compiled.mk
@@ -43,12 +43,13 @@ MAKE_ARGS+=	pch=off
 
 .include <bsd.port.options.mk>
 
-.if ${OSVERSION} > 1400000
+.if ${OPSYS} == FreeBSD && ${OSVERSION} >= 1400000 && ${OSVERSION} < 1400079
 USES+=	llvm:build
 CC=	${_LLVM_MK_PREFIX}/bin/clang
 CPP=	${_LLVM_MK_PREFIX}/bin/clang-cpp
 CXX=	${_LLVM_MK_PREFIX}/bin/clang++
-#LD?=	${_LLVM_MK_PREFIX}/bin/ld
+LD?=	${_LLVM_MK_PREFIX}/bin/ld
+CHOSEN_COMPILER_TYPE=	clang
 .else
 USES+=	compiler:c++17-lang
 .endif
